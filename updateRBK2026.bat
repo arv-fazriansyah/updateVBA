b69664aa7757ffd41424dc5dae2ab25a8e160b72
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2025-12-22 20:36:12 +0700

    Update updateRBK2026.bat

--- updateRBK2026.bat
+++ updateRBK2026.bat
@@ -190,7 +190,7 @@ start /wait "" "%exe%" a "%file%" "%source%\*" || (
 ::=============================================================
 ::  [10/10] Ganti nama file hasil update
 ::=============================================================
-echo   [10/10] Cleaning & Finishing...
+echo   [10/10] Cleaning ^& Finishing...
 set "batname=%version%"
 set "basename=%target%.xlsb"
 for /f "tokens=1,* delims=_" %%a in ("%basename%") do (
