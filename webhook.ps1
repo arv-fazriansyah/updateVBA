6ede6d1a49ba660be2666c9750cc72c7d5e7e85e
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2025-12-23 20:54:16 +0700

    Update webhook.ps1

--- webhook.ps1
+++ webhook.ps1
@@ -115,7 +115,7 @@ try {
             if ($targetWb) {
                 try {
                     # Jalankan macro pada workbook yang spesifik
-                    $targetWb.Parent.Run("TampilkanToast", "Judul", $pesan, "")
+                    $targetWb.Parent.Run("TampilkanToast", "", $pesan, "")
                 } catch {
                     Write-Log "ERROR: Gagal menjalankan Macro."
                 }
