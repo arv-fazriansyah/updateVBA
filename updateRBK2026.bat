307b2d2b705a7ba0d6ca5d93e901953c844cf036
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2025-12-24 21:07:17 +0700

    Update updateRBK2026.bat

--- updateRBK2026.bat
+++ updateRBK2026.bat
@@ -2,7 +2,6 @@
 setlocal enabledelayedexpansion
 color a
 
-"%SystemRoot%\System32\taskkill.exe" /f /im excel.exe >nul 2>nul
 ::=============================================================
 ::  Ambil nama file batch tanpa ekstensi (misal: v25.11.2025[MASTER_RBK2026])
 ::=============================================================
@@ -132,6 +131,8 @@ if not exist "%file%" (
 ::=============================================================
 echo   [5/10] Menutup file ARB...
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -command "$xl=[Runtime.InteropServices.Marshal]::GetActiveObject('Excel.Application'); $wb=$xl.Workbooks | Where-Object {$_.Name -like '*%target%*'}; if($wb){$wb.Close($false)}" >nul 2>&1
+"%SystemRoot%\System32\timeout.exe" /t 2 >nul
+"%SystemRoot%\System32\taskkill.exe" /f /im excel.exe >nul 2>nul
 "%SystemRoot%\System32\timeout.exe" /t 2 >nul
 
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -command "$xl=[Runtime.InteropServices.Marshal]::GetActiveObject('Excel.Application'); if($xl.Workbooks | Where-Object {$_.Name -like '*%target%*'}){ exit 1 } else { exit 0 }" >nul 2>&1
