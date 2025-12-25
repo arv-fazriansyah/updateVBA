c7d5fbfc32cddaf9cb9a54175d71a1dd31865e6c
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2025-12-25 14:34:39 +0700

    Update updateRBK2026.bat

--- updateRBK2026.bat
+++ updateRBK2026.bat
@@ -130,8 +130,9 @@ if not exist "%file%" (
 ::  Tutup file Excel target saja
 ::=============================================================
 echo   [5/10] Menutup file ARB...
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Get-CimInstance Win32_Process -Filter \"Name='excel.exe'\" | Where-Object { $_.CommandLine -like '*%target%*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }" >nul 2>&1
-"%SystemRoot%\System32\timeout.exe" /t 2 >nul
+::%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Get-CimInstance Win32_Process -Filter \"Name='excel.exe'\" | Where-Object { $_.CommandLine -like '*%target%*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }" >nul 2>&1
+::"%SystemRoot%\System32\timeout.exe" /t 2 >nul
+
 "%SystemRoot%\System32\taskkill.exe" /f /im excel.exe >nul 2>nul
 "%SystemRoot%\System32\timeout.exe" /t 2 >nul
 
